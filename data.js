234989b85ffd9ba96ca4b4a36327eff5989ef63e
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-02-19 21:45:47 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+const names = [
+ "Heather","Duncan","Gwen","Leshawna","Owen",
+ "Lindsay","Harold","Courtney","DJ","Bridgette",
+ "Izzy","Noah","Tyler","Alejandro","Sierra",
+ "Cody","Beth","Justin","Eva","Trent"
+];
+
+const eventPool = [
+ "%0 and %1 argue at camp.",
+ "%0 forms an alliance with %1.",
+ "%0 accidentally annoys everyone.",
+ "%0 starts targeting %1."
+];
